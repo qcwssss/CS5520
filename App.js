3897325089eaa24c433d46fd5fbc4e6fd73f691c
Chen Qiu <qcwssss@outlook.com>
2023-01-24 15:36:05 -0800

    added modal & buttons

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
 
@@ -9,16 +9,28 @@ export default function App() {
   const name = 'CS5520'
 
   const [enteredText, setEnteredText] = useState('')
+  const [modalVisible, setModalVisible] = useState(false);
 
   const onTextEnter = (textChanged) => {
     setEnteredText(textChanged)
+    setModalVisible(false)
+  }
+
+  const onCancel = () => {
+    setModalVisible(false)
   }
 
   return (
     <View style={styles.container}>
       <Text>Let's make some mobile app!</Text>
       <Header appName={name}/>
-      <Input textUpdateFunction={onTextEnter}/>
+      <Button title="Add task" onPress={() => {setModalVisible(true)}} />
+      <Input 
+      modalVisible={modalVisible}
+      onCancel={onCancel}
+       textUpdateFunction={onTextEnter}
+
+       />
       <StatusBar style="auto" />
       <Text>{enteredText}</Text>
     </View>
--- components/Input.js
+++ components/Input.js
@@ -1,24 +1,43 @@
-import { View, Text, Button, TextInput } from 'react-native'
+import { View, Button, TextInput, Modal, StyleSheet, Image } from 'react-native'
 
 import React, { useState } from 'react'
 
-const Input = ({textUpdateFunction}) => {
+const Input = ({modalVisible, textUpdateFunction, onCancel}) => {
 
   const [text,setText] = useState('')
 
   return (
-    <View>
-      <TextInput style={ {backgroundColor: "red"}} value={text} 
-      onChangeText={(changedText) => {
-        setText(changedText)
-      }}/>
+    <Modal visible={modalVisible}>
+      <View style={styles.container}>
+      <Image source={{uri: 'https://cdn-icons-png.flaticon.com/512/2617/2617812.png'}}
+       style={styles.image} />
+        <TextInput style={ {backgroundColor: "red"}} value={text} 
+        onChangeText={(changedText) => {
+          setText(changedText)
+        }}/>
+        <Button title='Confirm' onPress={()=> {
+          textUpdateFunction(text)
+          setText('')
+        }}></Button>
 
-      <Button title='Confirm' onPress={()=> {
-        textUpdateFunction(text)
-        setText('')
-      }}></Button>
-    </View>
+        <Button title='Cancel' onPress={onCancel} />
+
+      </View>
+      </Modal>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#aaa',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    height:100,
+    width:100
+  }
+});
 
 export default Input
