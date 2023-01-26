094ee41ac66a4fd48e719010046e6eb58f110237
Chen Qiu <qcwssss@outlook.com>
2023-01-26 14:52:15 -0800

    adding styles

--- App.js
+++ App.js
@@ -1,47 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
-import Input from './components/Input';
-
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Header from "./components/Header";
+import Input from "./components/Input";
 
 export default function App() {
-  const name = 'CS5520'
+  const name = "CS5520";
 
-  const [enteredText, setEnteredText] = useState('')
+  const [enteredText, setEnteredText] = useState("...");
   const [modalVisible, setModalVisible] = useState(false);
 
   const onTextEnter = (textChanged) => {
-    setEnteredText(textChanged)
-    setModalVisible(false)
-  }
+    setEnteredText(textChanged);
+    setModalVisible(false);
+  };
 
   const onCancel = () => {
-    setModalVisible(false)
-  }
+    setModalVisible(false);
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>Let's make some mobile app!</Text>
-      <Header appName={name}/>
-      <Button title="Add task" onPress={() => {setModalVisible(true)}} />
-      <Input 
-      modalVisible={modalVisible}
-      onCancel={onCancel}
-       textUpdateFunction={onTextEnter}
-
-       />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topContainer}>
+        <Text>Let's make some mobile app!</Text>
+        <Header appName={name} />
+        <Button
+          title="Add task"
+          onPress={() => {
+            setModalVisible(true);
+          }}
+        />
+      </View>
+      <Input
+        modalVisible={modalVisible}
+        onCancel={onCancel}
+        textUpdateFunction={onTextEnter}
+      />
       <StatusBar style="auto" />
-      <Text>{enteredText}</Text>
-    </View>
+      <Text style={styles.typedText}>You typed {enteredText}</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+  typedText: {
+    paddingTop: 40,
+    fontSize: 20,
+    color: "blue",
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  bottomContainer: {
+    flex: 4,
+    backgroundColor: "551255",
   },
 });
--- components/Input.js
+++ components/Input.js
@@ -1,43 +1,84 @@
-import { View, Button, TextInput, Modal, StyleSheet, Image } from 'react-native'
+import {
+  View,
+  Button,
+  TextInput,
+  Modal,
+  StyleSheet,
+  Image,
+} from "react-native";
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Input = ({modalVisible, textUpdateFunction, onCancel}) => {
-
-  const [text,setText] = useState('')
+const Input = ({ modalVisible, textUpdateFunction, onCancel }) => {
+  const [text, setText] = useState("");
 
   return (
     <Modal visible={modalVisible}>
       <View style={styles.container}>
-      <Image source={{uri: 'https://cdn-icons-png.flaticon.com/512/2617/2617812.png'}}
-       style={styles.image} />
-        <TextInput style={ {backgroundColor: "red"}} value={text} 
-        onChangeText={(changedText) => {
-          setText(changedText)
-        }}/>
-        <Button title='Confirm' onPress={()=> {
-          textUpdateFunction(text)
-          setText('')
-        }}></Button>
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/2617/2617812.png",
+          }}
+          style={styles.image}
+        />
 
-        <Button title='Cancel' onPress={onCancel} />
+        <TextInput
+          style={styles.input}
+          placeholder="Type something"
+          value={text}
+          onChangeText={(changedText) => {
+            setText(changedText);
+          }}
+        />
 
+        <View style={styles.btnContainer}>
+          <View style={styles.button}>
+            <Button
+              title="Confirm"
+              onPress={() => {
+                textUpdateFunction(text);
+                setText("");
+              }}
+            ></Button>
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={onCancel} />
+          </View>
+        </View>
       </View>
-      </Modal>
-  )
-}
+    </Modal>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#aaa',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
-    height:100,
-    width:100
-  }
+    height: 100,
+    width: 100,
+  },
+  input: {
+    margin: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 3,
+    padding: 10,
+    width: "40%",
+    borderBottomColor: "rebeccapurple",
+  },
+  btnContainer: {
+    flexDirection: "row",
+  },
+  button: {
+    backgroundColor: "#aaa",
+    marginHorizontal: 5,
+    fontSize: "10",
+    width: "30%",
+  },
 });
 
-export default Input
+export default Input;
