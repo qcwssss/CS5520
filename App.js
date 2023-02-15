2cefa521b8285933bc46bda0101ab236cf406c7c
Chen Qiu <qcwssss@outlook.com>
2023-02-14 16:27:47 -0800

    add header styles

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React from "react";
 import Home from "./Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import GoalDetail from "./components/GoalDetail";
 import Help from "./components/Help";
+import { AntDesign } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
-console.log(Stack);
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "plum" },
+          headerTintColor: "white",
+          headerTitleStyle: {
+            fontSize: 28,
+          },
+        }}
+      >
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="GoalDetail" component={GoalDetail} />
+        <Stack.Screen
+          options={({ route }) => {
+            return {
+              title: route.params.goalItem.text,
+              headerRight: () => {
+                return (
+                  <Pressable
+                    onPress={() => {
+                      console.log("> pressed");
+                    }}
+                  >
+                    <AntDesign name="rightcircleo" size={24} color="#eee" />
+                  </Pressable>
+                );
+              },
+            };
+          }}
+          name="GoalDetail"
+          component={GoalDetail}
+        />
         <Stack.Screen name="Help" component={Help} />
       </Stack.Navigator>
     </NavigationContainer>
