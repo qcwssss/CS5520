dac81e5e5438c264d3717c32a3fa552105110a10
Chen Qiu <qcwssss@outlook.com>
2023-02-14 20:46:34 -0800

    moved App.js content to Home.js, added Goal Details screen

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { View, Text, Pressable } from "react-native";
 import React from "react";
 import Home from "./Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import GoalDetail from "./components/GoalDetail";
 import Help from "./components/Help";
-import { AntDesign } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
@@ -23,22 +21,22 @@ const App = () => {
       >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen
-          options={({ route }) => {
-            return {
-              title: route.params.goalItem.text,
-              headerRight: () => {
-                return (
-                  <Pressable
-                    onPress={() => {
-                      console.log("> pressed");
-                    }}
-                  >
-                    <AntDesign name="rightcircleo" size={24} color="#eee" />
-                  </Pressable>
-                );
-              },
-            };
-          }}
+          // options={({ route }) => {
+          //   return {
+          //     title: route.params.goalItem.text,
+          //     headerRight: () => {
+          //       return (
+          //         <Pressable
+          //           onPress={() => {
+          //             console.log("> pressed");
+          //           }}
+          //         >
+          //           <AntDesign name="rightcircleo" size={24} color="#eee" />
+          //         </Pressable>
+          //       );
+          //     },
+          //   };
+          // }}
           name="GoalDetail"
           component={GoalDetail}
         />
--- components/GoalDetail.js
+++ components/GoalDetail.js
@@ -1,9 +1,29 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import { View, Text, Pressable } from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
 
 const GoalDetail = ({ route }) => {
-  console.log(route);
+  // console.log(route);
   const goal = route.params.goalItem;
+  const navigation = useNavigation();
+
+  const iconPressed = () => {
+    console.log("icon pressed from Goal Details");
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: goal.text,
+      headerRight: () => {
+        return (
+          <Pressable onPress={iconPressed}>
+            <AntDesign name="rightcircleo" size={24} color="#eee" />
+          </Pressable>
+        );
+      },
+    });
+  });
   return (
     <View>
       <Text>
