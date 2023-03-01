7b99b6d0c3a07096494ece2b05c7344a75968232
Chen Qiu <qcwssss@outlook.com>
2023-02-28 18:50:01 -0800

    add header style

--- components/GoalUsers.js
+++ components/GoalUsers.js
@@ -1,5 +1,6 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Button } from "react-native";
 import React, { useEffect, useState } from "react";
+import { async } from "@firebase/util";
 
 const GoalUsers = () => {
   const [users, setUsers] = useState([]);
@@ -24,6 +25,30 @@ const GoalUsers = () => {
     getUsers();
   }, []);
 
+  const addUser = async () => {
+    let url = "https://jsonplaceholder.typicode.com/posts";
+    try {
+      const response = await fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: "Benuser", id: Math.random() }), // body data type must match "Content-Type" header
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setUsers((prevUsers) => {
+          return [...prevUsers, data.name];
+        });
+      } else {
+        throw new Error("POST error");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <View>
       <FlatList
@@ -33,6 +58,7 @@ const GoalUsers = () => {
           return <Text>{item}</Text>;
         }}
       />
+      <Button title="Add User" onPress={addUser} />
     </View>
   );
 };
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, useWindowDimensions } from "react-native";
 import React from "react";
+import { Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const Header = (props) => {
+  const { height, width } = useWindowDimensions();
   return (
     <View>
       <Text style={styles.header}>Welcome to {props.appName}</Text>
@@ -15,7 +20,11 @@ const styles = StyleSheet.create({
     borderColor: "rebeccapurple",
     borderWidth: 2,
     padding: 5,
-    fontSize: 24,
+    // fontSize: 24,
+    fontSize: windowWidth < 380 ? 20 : 26,
+    paddingHorizontal: windowWidth < 380 ? 10 : 20,
+    width: 350,
+    maxWidth: "80%",
   },
 });
 
